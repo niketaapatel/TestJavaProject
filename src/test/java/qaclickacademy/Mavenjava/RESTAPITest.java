c3de2cd086aea706f10d2cbae80b48f745c358ee
niketaapatel <102696324+niketaapatel@users.noreply.github.com>
2022-04-22 10:25:32 -0400

    commit by B

--- src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -8,14 +8,14 @@ public class RESTAPITest {
 	public void postJira()
 	{
 		System.out.println("postJira");
-		System.out.println("Test1");
-		System.out.println("Test1");
+		System.out.println("NiketaRetest1");
+		
 	}
 	@Test
 	public void deleteTwitter()
 	{
 		System.out.println("deleteTwitter");
-		System.out.println("deleteTwitter");
-		System.out.println("deleteTwitter");
+		System.out.println("NiketaRetest1");
+		
 	}
 }
