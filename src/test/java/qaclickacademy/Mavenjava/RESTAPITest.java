258e8aad8f54de14965751dbe38b6120e68c65b8
niketaapatel <102696324+niketaapatel@users.noreply.github.com>
2022-04-21 22:52:15 -0400

    changes

--- src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -15,5 +15,7 @@ public class RESTAPITest {
 	public void deleteTwitter()
 	{
 		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
 	}
 }
